b54675f552e84bb44c4bbce6e211a794455f509e
Uon Liaquat <antelopes06@gmail.com>
2021-07-06 00:05:24 +0500

    Set Graph and tested both algorithms

--- agent.py
+++ agent.py
@@ -7,7 +7,7 @@ random.seed(10)
 
 class Agent:
     def __init__(self, no_of_states, no_of_actions, alpha=0.01, gamma=1.0, eps=1.0, eps_min=0.001,
-                 eps_decay=0.9999):
+                 eps_decay=0.999999, off_policy=True):
         self.no_of_states = no_of_states
         self.no_of_actions = no_of_actions
         self.alpha = alpha
@@ -15,6 +15,7 @@ class Agent:
         self.eps = eps
         self.eps_min = eps_min
         self.eps_decay = eps_decay
+        self.off_policy = off_policy
         self.Q = defaultdict(lambda: np.zeros(no_of_actions))
 
     def choose_action(self, state):
@@ -24,13 +25,27 @@ class Agent:
             return np.argmax(self.Q[state])
 
     # Q[s][a] = Q[s][a] + alpha * (reward + gamma * maxAV(s_) - Q[s][a])
-    def learn(self, s, a, r, s_):
-        self.Q[s][a] = self.Q[s][a] + (self.alpha * ((r + self.gamma * np.max(self.Q[s_])) - self.Q[s][a]))
-        self.decay_eps()
-
-    def learn(self, s, a, r, s_, a_):
-        self.Q[s][a] = self.Q[s][a] + (self.alpha * ((r + self.gamma * self.Q[s_][a_]) - self.Q[s][a]))
+    def learn(self, s, a, r, s_, a_=None):
+        if self.off_policy:
+            self.Q[s][a] = self.Q[s][a] + (self.alpha * ((r + self.gamma * np.max(self.Q[s_])) - self.Q[s][a]))
+        else:
+            self.Q[s][a] = self.Q[s][a] + (self.alpha * ((r + self.gamma * self.Q[s_][a_]) - self.Q[s][a]))
         self.decay_eps()
 
     def decay_eps(self):
         self.eps = self.eps * self.eps_decay if self.eps > self.eps_min else self.eps_min
+
+    def max_action(self, state):
+        return np.argmax(self.Q[state])
+
+    def extract_policy(self):
+        policy = defaultdict(lambda: 0)
+        for k, action_values in self.Q.items():
+            action = 0
+            max_val = self.Q[k][action]
+            for a, v in enumerate(action_values):
+                if v > max_val:
+                    max_val = v
+                    action = a
+            policy[k] = action
+        return policy
--- main.py
+++ main.py
@@ -8,10 +8,10 @@ from collections import deque
 plt.style.use('default')
 
 env = gym.make('CliffWalking-v0')
-agent = Agent(no_of_states=env.observation_space.n, no_of_actions=env.action_space.n)
+agent = Agent(no_of_states=env.observation_space.n, no_of_actions=env.action_space.n, off_policy=False)
 
 fig, ax = plt.subplots(2)
-fig.suptitle('QLearning')
+
 
 ax[0].set_xlabel("Episodes")
 ax[0].set_ylabel("Mean Score")
@@ -23,13 +23,20 @@ ax[1].set_ylabel("Epsilon")
 # ax[1].set_ylim(0.00, 1.00)
 
 
-def animate(i, x_values, y_values_score, y_values_eps):
+def animate(i, ep, x_values, y_values_score, y_values_eps):
+    if len(x_values) > 20:
+        x_values.pop(0)
+        y_values_score.pop(0)
+        y_values_eps.pop(0)
+        ax[0].cla()
+        ax[1].cla()
+
     ax[0].plot(x_values, y_values_score, linestyle='-')
     ax[1].plot(x_values, y_values_eps, linestyle='-')
     pass
 
 
-def plot_graphs(ep, scores_list, x_values, y_values_score, y_values_eps):
+def cal_graph_vals(ep, scores_list, x_values, y_values_score, y_values_eps):
     mean = np.mean(scores_list)
     scores_list.clear()
     x_values.append(ep)
@@ -37,11 +44,29 @@ def plot_graphs(ep, scores_list, x_values, y_values_score, y_values_eps):
     y_values_eps.append(agent.eps)
 
 
+def test_agent():
+    score = 0
+    state = env.reset()
+    done = False
+    steps = 0
+    while not done and steps < 50:
+        steps += 1
+        action = agent.max_action(state)
+        state_, reward, done, _ = env.step(action)
+        print(state, action, state_, reward)
+        state = state_
+        score += reward
+    print('\n\n\n')
+    return np.mean(score)
+
+
 def QLearning(episodes=50000, plot_every=10):
-    x_values = deque(maxlen=episodes)
-    y_values_score = deque(maxlen=episodes)
-    y_values_eps = deque(maxlen=episodes)
-    scores_list = deque(maxlen=100)
+    fig.suptitle('QLearning')
+    x_values = []
+    y_values_score = []
+    y_values_eps = []
+    scores_list = deque(maxlen=plot_every)
+    count = 0
     for ep in range(1, episodes + 1):
         score = 0
         state = env.reset()
@@ -54,17 +79,18 @@ def QLearning(episodes=50000, plot_every=10):
             score += reward
         scores_list.append(score)
         if (ep % plot_every) == 0:
-            plot_graphs(ep, scores_list, x_values, y_values_score, y_values_eps)
+            cal_graph_vals(ep, scores_list, x_values, y_values_score, y_values_eps)
             plt.pause(0.0001)
             plt.tight_layout()
-            ani = FuncAnimation(plt.gcf(), animate, fargs=(x_values, y_values_score, y_values_eps))
+            ani = FuncAnimation(plt.gcf(), animate, fargs=(ep, x_values, y_values_score, y_values_eps))
     plt.show()
 
 
 def Sarsa(episodes=50000, plot_every=10):
-    x_values = deque(maxlen=episodes)
-    y_values_score = deque(maxlen=episodes)
-    y_values_eps = deque(maxlen=episodes)
+    fig.suptitle('Sarsa')
+    x_values = []
+    y_values_score = []
+    y_values_eps = []
     scores_list = deque(maxlen=100)
     for ep in range(1, episodes + 1):
         score = 0
@@ -79,10 +105,10 @@ def Sarsa(episodes=50000, plot_every=10):
             score += reward
         scores_list.append(score)
         if (ep % plot_every) == 0:
-            plot_graphs(ep, scores_list, x_values, y_values_score, y_values_eps)
+            cal_graph_vals(ep, scores_list, x_values, y_values_score, y_values_eps)
             plt.pause(0.0001)
             plt.tight_layout()
-            ani = FuncAnimation(plt.gcf(), animate, fargs=(x_values, y_values_score, y_values_eps))
+            ani = FuncAnimation(plt.gcf(), animate, fargs=(ep, x_values, y_values_score, y_values_eps))
     plt.show()
 
 
